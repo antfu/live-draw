57eabc777eb259ea17f46e831ed64499fa691d78
Anthony Fu <anthonyfu117@hotmail.com>
2015-12-19 19:58:03 +0800

    UI optimized

--- FreeDraw/MainWindow.xaml.cs
+++ FreeDraw/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Ink;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace AntFu7.FreeDraw
 {
@@ -14,26 +17,90 @@ namespace AntFu7.FreeDraw
     {
         private Point _lastMousePosition;
         private bool _isDraging = false;
-        private Border _selectedColor;
+        private Button _selectedColor;
         private string _staticInfo = "";
         private bool _displayingInfo;
-        private bool _usingEraser;
+        private bool _displayDetialPanel;
+        private bool _eraserMode;
+        private bool _enable;
+
+        //Background Brushs for Buttons
+        private readonly SolidColorBrush _transparentBrush = new SolidColorBrush(Colors.Transparent);
+        private readonly SolidColorBrush _fakeBrush = new SolidColorBrush(Color.FromArgb(1, 0, 0, 0));
+
+
+        #region Mouse Throught
+
+        private const int WsExTransparent = 0x20;
+        private const int GwlExstyle = (-20);
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int nIndex);
+
+        private void SetThrought(bool t)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            var extendedStyle = GetWindowLong(hwnd, GwlExstyle);
+            if (t)
+                SetWindowLong(hwnd, GwlExstyle, extendedStyle | WsExTransparent);
+            else
+                SetWindowLong(hwnd, GwlExstyle, extendedStyle & ~(uint)WsExTransparent);
+        }
+
+        private void ToggleDetial(bool v)
+        {
+            if (v)
+            {
+                DetialTogglerRotate.Angle = 90;
+                SliderPanel.Visibility=Visibility.Visible;
+                ButtonPanel.Visibility=Visibility.Visible;
+            }
+            else
+            {
+                DetialTogglerRotate.Angle = 270;
+                SliderPanel.Visibility = Visibility.Collapsed;
+                ButtonPanel.Visibility = Visibility.Collapsed;
+            }
+            _displayDetialPanel = v;
+        }
+        private void SetEnable(bool b)
+        {
+            if (b)
+            {
+                EnableButton.Tag = "Actived";
+                Background = _fakeBrush;
+            }
+            else
+            {
+                EnableButton.Tag = null;
+                Background = _transparentBrush;
+            }
+            _enable = b;
+        }
+        #endregion
 
         public MainWindow()
         {
             InitializeComponent();
             SelectColor(DefaultColor);
+            SetEnable(true);
+            ToggleDetial(false);
         }
 
-        private void SelectColor(Border b)
+
+        private void SelectColor(Button b)
         {
+            if (ReferenceEquals(_selectedColor, b)) return;
             var solidColorBrush = b.Background as SolidColorBrush;
             if (solidColorBrush == null) return;
             MainInkCanvas.DefaultDrawingAttributes.Color = solidColorBrush.Color;
             brushPreview.Background = b.Background;
-            b.BorderThickness = new Thickness(2);
+            b.Tag = "Selected";
             if (_selectedColor != null)
-                _selectedColor.BorderThickness = new Thickness(0);
+                _selectedColor.Tag = null;
             _selectedColor = b;
         }
 
@@ -83,62 +150,59 @@ namespace AntFu7.FreeDraw
                 InfoBox.Text = _staticInfo;
         }
 
-
         #region Controls
-        private void ColorSelector_MouseDown(object sender, MouseEventArgs e)
+        private void ColorSelector_MouseDown(object sender, RoutedEventArgs e)
         {
-            var border = sender as Border;
+            var border = sender as Button;
             if (border == null) return;
             SelectColor(border);
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            MainInkCanvas.DefaultDrawingAttributes.Height = e.NewValue;
-            MainInkCanvas.DefaultDrawingAttributes.Width = e.NewValue;
-            brushPreview.Height = e.NewValue;
-            brushPreview.Width = e.NewValue;
+            var s = e.NewValue * e.NewValue;
+            MainInkCanvas.DefaultDrawingAttributes.Height = s;
+            MainInkCanvas.DefaultDrawingAttributes.Width = s;
+            brushPreview.Height = s;
+            brushPreview.Width = s;
         }
-        private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void CloseButton_MouseDown(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown(0);
         }
 
-        private void EraserButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void EraserButton_MouseDown(object sender, RoutedEventArgs e)
         {
-            if (_usingEraser)
+            if (_eraserMode)
             {
                 MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
-                //EraserButton.BorderThickness = new Thickness(1);
-                EraserButton.Background = new SolidColorBrush(Color.FromArgb(0x4C, 0, 0, 0));
-                _usingEraser = false;
+                EraserButton.Tag = null;
+                _eraserMode = false;
                 SetStaticInfo("");
             }
             else
             {
                 MainInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
-                //EraserButton.BorderThickness = new Thickness(2);
-                EraserButton.Background = new SolidColorBrush(Color.FromArgb(80, 255, 255, 255));
-                _usingEraser = true;
+                EraserButton.Tag = "Actived";
+                _eraserMode = true;
                 SetStaticInfo("Eraser Mode");
             }
         }
 
-        private void UndoButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void UndoButton_MouseDown(object sender, RoutedEventArgs e)
         {
             Display("Function Unfinished");
         }
 
-        private void SaveButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void SaveButton_MouseDown(object sender, RoutedEventArgs e)
         {
             var fn = "Test.fdw";
             using (var s = GetFileStream(fn))
                 MainInkCanvas.Strokes.Save(s);
             Display("Saved to " + fn);
-
         }
 
-        private void LoadButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void LoadButton_MouseDown(object sender, RoutedEventArgs e)
         {
             var fn = "Test.fdw";
             using (var s = GetFileStream(fn))
@@ -146,12 +210,12 @@ namespace AntFu7.FreeDraw
             Display("Loaded from " + fn);
         }
 
-        private void MinimizeButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void MinimizeButton_MouseDown(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
         }
 
-        private void ClearButton_MouseDown(object sender, MouseButtonEventArgs e)
+        private void ClearButton_MouseDown(object sender, RoutedEventArgs e)
         {
             MainInkCanvas.Strokes.Clear();
             Display("Cleared");
@@ -192,5 +256,15 @@ namespace AntFu7.FreeDraw
         #endregion
 
         #endregion
+
+        private void EnableButton_MouseDown(object sender, RoutedEventArgs e)
+        {
+            SetEnable(!_enable);
+        }
+
+        private void DetialToggler_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleDetial(!_displayDetialPanel);
+        }
     }
 }

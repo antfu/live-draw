5f2c9c2b75465c5e8edf11d4f436234199510274
true_mogician <truemogician@gmail.com>
2023-04-15 00:36:45 +0800

    feat: Support multiple monitors
    Set the bounding of window to cover all monitors and place the palette in the center of the primary screen when the window is loaded. This process will be executed again when the screen settings are changed.

--- LiveDraw/MainWindow.xaml.cs
+++ LiveDraw/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using Brush = System.Windows.Media.Brush;
 using Point = System.Windows.Point;
+using Screen = System.Windows.Forms.Screen;
 
 namespace AntFu7.LiveDraw
 {
@@ -97,6 +98,13 @@ namespace AntFu7.LiveDraw
                 Application.Current.Shutdown(0);
             }
         }
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			AdjustWindowSize();
+			Microsoft.Win32.SystemEvents.DisplaySettingsChanged += (_, _) => AdjustWindowSize();
+		}
 
         private void Exit(object sender, EventArgs e)
         {
@@ -408,6 +416,22 @@ namespace AntFu7.LiveDraw
                 EraseByPoint_Flag = (int)erase_mode.NONE;
             }
         }
+
+		private void AdjustWindowSize()
+		{
+			var primaryArea = Screen.PrimaryScreen.WorkingArea;
+			var scaleRatio = Math.Max(
+				primaryArea.Width / SystemParameters.PrimaryScreenWidth,
+				primaryArea.Height / SystemParameters.PrimaryScreenHeight
+			);
+			Left = Screen.AllScreens.Min(s => s.WorkingArea.Left) / scaleRatio;
+			Top = Screen.AllScreens.Min(s => s.WorkingArea.Top) / scaleRatio;
+			Width = Screen.AllScreens.Max(s => s.WorkingArea.Right) / scaleRatio - Left;
+			Height = Screen.AllScreens.Max(s => s.WorkingArea.Bottom) / scaleRatio - Top;
+
+			Canvas.SetTop(Palette, (primaryArea.Top + primaryArea.Height / 2) / scaleRatio - Top - Palette.ActualHeight / 2);
+			Canvas.SetLeft(Palette, (primaryArea.Left + primaryArea.Width / 2) / scaleRatio - Left - Palette.ActualWidth / 2);
+		}
         #endregion
 
 
